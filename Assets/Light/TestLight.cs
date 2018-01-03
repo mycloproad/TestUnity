0dfe683ee232d9743f2e18d6f49061c014012600
chenle <mycloproad@163.com>
2018-01-03 18:46:25 +0800

    测试

---
+++ Assets/Light/TestLight.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestLight : MonoBehaviour {
+
+	Light aa;
+	void Start () {
+		aa = GetComponent<Light>();
+		aa.type = LightType.Point;
+		//cullingMask 的设置问题
+		aa.cullingMask = 5;
+	}
+	
+	void Update () {
+		
+	}
+}
